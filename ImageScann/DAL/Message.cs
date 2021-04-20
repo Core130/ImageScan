41f3a5920d83e0e757fa1d85851186c225a0e1ed
Core130 <1114968919@qq.com>
2021-04-20 18:11:18 +0800

    文件重命名

---
+++ ImageScann/DAL/Message.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+namespace ImageScann.DAL
+{
+    public class Message
+    {
+        public string MsgContent { get; set; }
+        public string CrtDate { get; set; }
+        public int ID { get; set; }
+        public string FromUser { get; set; }
+        public string DataNbr { get; set; }
+        public bool? bRead { get; set; }
+    }
+}
